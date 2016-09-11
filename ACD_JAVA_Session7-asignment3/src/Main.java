58723e54888c43e5693f5b3ca1210a8a947a4387
u174762 <sam.panga@hotmail.com>
2016-09-11 10:25:16 +0530

    Add files via upload

---
+++ ACD_JAVA_Session7-asignment3/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	private static final String s = null;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			String S=null;
+			System.out.println(S.length());
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Null Pointer exception is caught");
+		
+		}
+
+	}
+
+}
